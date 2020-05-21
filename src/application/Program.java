2942e0df77d1ba316afec81ec80911d3a644696a
_lucasborges <lucas.borges@dbccompany.com.br>
2020-05-21 18:40:56 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,10 +13,12 @@ public class Program {
 
     Department obj = new Department( 1, "Books" );
 
-    Seller seller = new Seller( 21, "bob", "bob@gmail.com", new Date(), 3000.00, obj );
+    //Seller seller = new Seller( 21, "bob", "bob@gmail.com", new Date(), 3000.00, obj );
 
     SellerDao sellerDao = DaoFactory.createSellerDao();
-    
+
+    Seller seller = sellerDao.findById( 3 );
+
     System.out.println(seller);
   }
 }
