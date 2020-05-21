c1a87ab9479b9d5418fcef4f6b8fab951bf32a34
_lucasborges <lucas.borges@dbccompany.com.br>
2020-05-21 18:52:48 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,9 @@ public class Program {
 
   public static void main( String[] args ) {
 
-    Department obj = new Department( 1, "Books" );
-
-    //Seller seller = new Seller( 21, "bob", "bob@gmail.com", new Date(), 3000.00, obj );
-
     SellerDao sellerDao = DaoFactory.createSellerDao();
 
+    System.out.println( "=== TEST 1: sellerById ===" );
     Seller seller = sellerDao.findById( 3 );
 
     System.out.println(seller);
